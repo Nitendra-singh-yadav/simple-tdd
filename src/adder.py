8ee54ff8345f5d1c3c4f824652815993caf5de79
Nitendra Singh Yadav <nitendrasinghyadav@Nitendras-MacBook-Air.local>
2024-02-17 11:26:25 +0530

    added code to handle newline instead comma

--- src/adder.py
+++ src/adder.py
@@ -1,5 +1,6 @@
 def add(num_string):
     if not num_string:
         return 0
+    num_string = num_string.replace('\n', ',')
     nums = list(map(int, num_string.split(',')))
     return sum(nums)
