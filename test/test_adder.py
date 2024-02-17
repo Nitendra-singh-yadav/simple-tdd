5e9051049d1aad3692ac8d543a6e8f547dbeff8f
Nitendra Singh Yadav <nitendrasinghyadav@Nitendras-MacBook-Air.local>
2024-02-17 11:18:36 +0530

    added test for simple success cases

--- test/test_adder.py
+++ test/test_adder.py
@@ -1,1 +1,10 @@
-from src import adder
+from src.adder import add
+
+def test_empty_string():
+    assert add('') == 0
+
+def test_single_number():
+    assert add('1') == 1
+
+def test_two_numbers():
+    assert add('1,2') == 3
